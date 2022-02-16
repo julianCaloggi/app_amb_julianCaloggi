55624ca030f98ef0680a650b568fd51f4ebb8108
julianCaloggi <julianCaloggi@hotmail.com>
2022-02-16 16:17:52 -0300

    tercera entrega

--- App.js
+++ App.js
@@ -1,198 +1,44 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  TextInput,
-  ScrollView,
-  TouchableOpacity,
-  Image,
-  FlatList,
-  Modal,
-} from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { useState } from "react";
-import AddItemList from "./components/addItemList/addItemList";
+import Computer from "./components/computer";
+import Console from "./components/console";
 
 export default function App() {
-  const [InputText, setInputText] = useState("");
-  const [ItemsList, setItemsList] = useState([]);
-  const [ItemSelect, setItemSelect] = useState({});
-  const [Visiviliti, setVisiviliti] = useState(false);
+  const [changePage, setChangePage] = useState();
 
-  const TextController = (text) => {
-    setInputText(text);
+  const HandlerChangePage = (change) => {
+    setChangePage(change);
   };
 
-  const TextButtonInputList = () => {
-    setInputText("");
-    setItemsList([
-      ...ItemsList,
-      { value: InputText, id: Math.random().toString() },
-    ]);
-  };
+  let content = <Computer onChangePage={HandlerChangePage} />;
 
-  const ItemtDelete = (item) => {
-    setVisiviliti(true);
-    setItemSelect(item);
-  };
+  if (changePage == true) {
+    content = <Console />;
+  }
 
-  const confirmDelete = () => {
-    const { id } = ItemSelect;
-    setItemsList(ItemsList.filter((item) => item.id !== id));
-    setVisiviliti(false);
-    setItemSelect({});
-  };
-
-  const NoconfirmDelete = () => {
-    setVisiviliti(false);
-  };
-
-  return (
-    <View style={styles.container}>
-      <AddItemList
-        TextInput={InputText}
-        TextController={TextController}
-        TextButtonInputList={TextButtonInputList}
-      />
-
-      <FlatList
-        style={styles.container_items_list}
-        data={ItemsList}
-        renderItem={({ item }) => {
-          return (
-            <View key={item.id} style={styles.items_list}>
-              <Text style={styles.name_item}>{item.value}</Text>
-              <TouchableOpacity
-                style={styles.button_delete_items}
-                onPress={() => ItemtDelete(item)}
-              >
-                <Text style={styles.text_button_delete_items}>Borrar</Text>
-              </TouchableOpacity>
-            </View>
-          );
-        }}
-      />
-
-      <Modal
-        style={styles.container_modal}
-        animationType="slide"
-        visible={Visiviliti}
-      >
-        <View style={styles.container_modal_view}>
-          <Text style={styles.text_modal}>
-            ¿Esta seguro que desea eliminar?
-          </Text>
-          <Text style={styles.text_modal_item}>{ItemSelect.value}</Text>
-        </View>
-        <View style={styles.view_button_modal}>
-          <TouchableOpacity
-            style={styles.button_modal_yes}
-            onPress={confirmDelete}
-          >
-            <Text style={styles.text_button_modal}>SI</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button_modal_no}
-            onPress={NoconfirmDelete}
-          >
-            <Text style={styles.text_button_modal}>NO</Text>
-          </TouchableOpacity>
-        </View>
-      </Modal>
-    </View>
-  );
+  return <View>{content}</View>;
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#ffffff",
-    height: "100%",
-    margin: 20,
+    margin: 10,
+    marginTop: 40,
   },
-
-  container_items_list: {
-    marginTop: 10,
-    marginLeft: 10,
-    marginRight: 10,
-    marginBottom: 40,
-  },
-
-  items_list: {
+  container_cards: {
     flexDirection: "row",
-    justifyContent: "space-between",
-    backgroundColor: "#D3D3D3",
+  },
+
+  button_consola: {
     borderRadius: 10,
-    borderColor: "black",
     borderWidth: 2,
-    padding: 15,
-    marginTop: 5,
-  },
-
-  name_item: {
-    fontWeight: "bold",
-    fontSize: 16,
-    width: "40%",
-    padding: 10,
-    borderColor: "black",
-    borderWidth: 2,
-    backgroundColor: "white",
-  },
-
-  button_delete_items: {
-    borderRadius: 10,
-    width: "30%",
-    backgroundColor: "white",
-    borderColor: "black",
-    borderWidth: 2,
-  },
-
-  text_button_delete_items: {
-    fontWeight: "bold",
-    textAlignVertical: "center",
-    textAlign: "center",
+    borderColor: "#0000ff",
     padding: 10,
   },
-  text_modal: {
-    textAlign: "center",
-    fontSize: 16,
-    fontWeight: "bold",
-    margin: 10,
-  },
 
-  text_modal_item: {
+  text_button_consola: {
     textAlign: "center",
     fontSize: 16,
     fontWeight: "bold",
-    margin: 10,
     color: "red",
-  },
-
-  view_button_modal: {
-    flexDirection: "row",
-    textAlign: "center",
-    justifyContent: "space-around",
-    margin: 20,
-  },
-
-  button_modal_yes: {
-    borderRadius: 10,
-    backgroundColor: "#32cd32",
-    padding: 20,
-    borderColor: "green",
-    borderWidth: 2,
-  },
-
-  button_modal_no: {
-    borderRadius: 10,
-    backgroundColor: "#ff0000",
-    padding: 20,
-    borderColor: "red",
-    borderWidth: 2,
-  },
-
-  text_button_modal: {
-    fontWeight: "bold",
-    fontSize: 16,
-    color: "white",
   },
 });
---
+++ components/styles_computer_console.js
@@ -0,0 +1,28 @@
+import react from "react";
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+    marginTop: 40,
+  },
+  container_cards: {
+    flexDirection: "row",
+  },
+
+  button_consola: {
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "#0000ff",
+    padding: 10,
+  },
+
+  text_button_consola: {
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "red",
+  },
+});
+
+export default styles;
