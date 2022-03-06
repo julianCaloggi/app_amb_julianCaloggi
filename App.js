b5b7ca258e354be27e3c8a6760e3c2c58d04e14e
julianCaloggi <julianCaloggi@hotmail.com>
2022-03-06 17:48:26 -0300

    entrega de redux

--- App.js
+++ App.js
@@ -1,6 +1,24 @@
+import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import NavigationApp from "./navigation/navigatioApp";
+import TodoApp from "./src/TodoApp";
+import store from "./src/store";
+import { Provider } from "react-redux";
 
-export default function App() {
-  return <NavigationApp />;
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <TodoApp />
+      </Provider>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+});
